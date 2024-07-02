9a4b2d62f1b4fded90c63c507ff704925d444149
LauraRJ <romerojaque.laura@gmail.com>
2024-07-02 09:54:39 +0200

    Update and rename ejercicios clase.m to codigo.m

--- 5/Código/codigo.m
+++ 5/Código/codigo.m
@@ -1,4 +1,4 @@
-ejercicios
+
 function imout = segmentacion_disco_mrk(imin)
 
 %Componente roja
@@ -12,7 +12,7 @@ imc=imclose(imr,s);
 % Umbralizamos
 imc_umbral= imc > 170;
 
-% Buscamos el objeto más grande
+% Buscamos el objeto mÃ¡s grande
 %imBW=bwconncomp(imc_umbral);
 imBW=bwlabel(imc_umbral);
 imAreas=regionprops(imBW,'Area');
@@ -48,7 +48,7 @@ im_D=bwdist(imu_N);
 figure('Name','imu_D')
 imshow(mat2gray(im_D))
 
-% Buscamos el pixel máximo de la imagen
+% Buscamos el pixel mÃ¡ximo de la imagen
 ma=max(max(im_D));
 [x,y]=find(im_D==ma);
 imPixel=zeros(size(imBW,1),size(imBW,2));
@@ -88,10 +88,10 @@ s=strel('square',30);
 imc=imclose(imr,s);
 
 %% Calculamos el gradiente 
-% Aplicamos una dilatación
+% Aplicamos una dilataciÃ³n
 imd=imdilate(imc,s);
 imc2=imclose(imc,s);
-% Se obtiene el gradiente haciendo la diferencia entre la dilatación y el
+% Se obtiene el gradiente haciendo la diferencia entre la dilataciÃ³n y el
 % cierre de la imagen imc
 img= imsubtract(imd,imc2);
 
@@ -99,9 +99,9 @@ img= imsubtract(imd,imc2);
 imout=watershed(img);
 
 %% Finalmente visualizamos la imagen 
-figure ('Name','Segmentación de disco')
+figure ('Name','SegmentaciÃ³n de disco')
 subplot(2,2,1),imshow(imr), title('Canal rojo')
-subplot(2,2,2),imshow(imc), title('Eliminamos los vasos sanguíneos')
+subplot(2,2,2),imshow(imc), title('Eliminamos los vasos sanguÃ­neos')
 subplot(2,2,3),imshow(img), title('Gradiente de imc')
 subplot(2,2,4), imshow(imout), title('Imagen segmentada')
 
